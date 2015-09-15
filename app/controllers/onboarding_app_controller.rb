529b1a3b418cb6b2409d9b0ad751a66e8cb37ddf
Zach <briggszj@gmail.com>
2015-09-14 21:55:01 -0500

    Wrap injected index in rails view

--- app/controllers/onboarding_app_controller.rb
+++ app/controllers/onboarding_app_controller.rb
@@ -1,15 +1,13 @@
-class RegisterAppController < ApplicationController
+class OnboardingAppController < ApplicationController
   def index
     app_name = "businessdotcom-onboarding:"
     if params[:index_key]
       redis_key = app_name + params[:index_key]
-      payload = $redis.get(redis_key)
+      @payload = $redis.get(redis_key)
     else
       redis_key = app_name + "current"
       index = $redis.get(redis_key)
-      payload = $redis.get(index)
+      @payload = $redis.get(index)
     end
-
-    render text: payload
   end
 end
